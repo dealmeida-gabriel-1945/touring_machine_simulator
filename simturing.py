25c74116a3393ce7c0b435f709a20dff5d419866
Gabriel Guimarães <dealmeida.gabriel.d@gmail.com>
2022-01-03 15:11:33 -0300

    sum of 9

--- simturing.py
+++ simturing.py
@@ -12,17 +12,16 @@ def main():
     debug = 1
 
     if debug:
-        parameters.last_new_instructions = ['-r', '-s', '400']
+        parameters.last_new_instructions = ['-r', '-s', '40000']
         parameter_util.handle_args(parameters.last_new_instructions)
         print("\n Simulador de Máquina de Turing ver 1. 0 \n Desenvolvido como trabalho prático para a disciplina de "
               f"Teoria da Computação \n {author}, {institution}, {year}")
-        for y in range(0, 10):
-            parameters.first_word = f'8+{y}='
-            print(parameters.first_word)
-            mt = file_util.retrieve_mt_from_file('/home/gabriel/Documents/projects/python/touring_machine_simulator/examples/example_02.txt')
-            mt.accept(parameters.first_word)
-            if parameters.run_resume:
-                print(f'Conteúdo final na fita: {mt.get_final_tape_content()}')
+        parameters.first_word = f'1000+10000000009000='
+        print(parameters.first_word)
+        mt = file_util.retrieve_mt_from_file('/home/gabriel/Documents/projects/python/touring_machine_simulator/examples/example_02.txt')
+        mt.accept(parameters.first_word)
+        if parameters.run_resume:
+            print(f'Conteúdo final na fita: {mt.get_final_tape_content()}')
     else:
         parameters.last_new_instructions = sys.argv[1:len(sys.argv)]
         parameter_util.handle_args(parameters.last_new_instructions)
